e08a815c567f6e4b56830d9d315865fe50ec672f
fulviocanducci <fulviocanducci@hotmail.com>
2017-10-18 11:38:03 -0200

    Example Web Postgres

---
+++ PostgresWeb/Models/Credit.cs
@@ -0,0 +1,18 @@
+﻿namespace PostgresWeb.Models
+{
+    public class Credit
+    {
+        public Credit() { }
+        public Credit(string description)
+        {         
+            Description = description;
+        }
+        public Credit(int id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
